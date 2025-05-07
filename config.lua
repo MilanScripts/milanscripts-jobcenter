a82ec47cc58befd30a38421be37e2813930759b6
MilanScripts <milanscripts@gmail.com>
2025-05-07 15:58:37 +0200

    Fixed cooldown time to be resonable

--- config.lua
+++ config.lua
@@ -18,7 +18,7 @@ Config.Blip = {
     scale = 0.7
 }
 
-Config.CooldownTime = 3 -- Cooldown time in seconds for job change requests
+Config.CooldownTime = 1 -- Cooldown time in seconds for job change requests
 Config.Webhook = "X" -- Discord webhook for logging job changes
 
 Config.Jobs = {

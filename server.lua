5442c4be318a133f5501a58b9800770a24b0ffc6
MilanScripts <milanscripts@gmail.com>
2025-05-07 15:53:55 +0200

    Added logs (webhooks)

--- server.lua
+++ server.lua
@@ -12,7 +12,7 @@ RegisterServerEvent('milanscripts:setJob', function(jobName)
         DropPlayer(src, 'You are triggering this event too frequently.')
         return
     end
-    
+
     cooldowns[src] = currentTime
 
     local valid = false
@@ -24,16 +24,60 @@ RegisterServerEvent('milanscripts:setJob', function(jobName)
     end
 
     if not valid then
-        print(('[milanscripts:setJob] Invalid job attempt by player %s (ID: %d) for job: %s'):format(xPlayer.getName(), src, jobName))
+        print(('[milanscripts:setJob] Invalid job attempt by player %s (ID: %d) for job: %s'):format(xPlayer.getName(),
+            src, jobName))
 
         if Config.AntiAbuse.mode == 'drop' then
+            local embed = {
+                {
+                    ["color"] = 16711680,
+                    ["title"] = "Trigger Abuse",
+                    ["description"] = "Player: " ..
+                    xPlayer.getName() .. "\nID: " .. src .. "\nTrigger: milanscripts:setJob\nJob: " .. jobName,
+                    ["footer"] = {
+                        ["text"] = os.date("%Y-%m-%d %H:%M:%S"),
+                    },
+                }
+            }
+            PerformHttpRequest(Config.AntiAbuse.webhook, function(err, text, headers) end, 'POST',
+                json.encode({ username = "MilanScripts | Job Center", embeds = embed }),
+                { ['Content-Type'] = 'application/json' })
             DropPlayer(src, 'Trigger abuse detected.')
         elseif Config.AntiAbuse.mode == 'trigger' then
+            local embed = {
+                {
+                    ["color"] = 16711680,
+                    ["title"] = "Trigger Abuse",
+                    ["description"] = "Player: " ..
+                    xPlayer.getName() .. "\nID: " .. src .. "\nTrigger: milanscripts:setJob\nJob: " .. jobName,
+                    ["footer"] = {
+                        ["text"] = os.date("%Y-%m-%d %H:%M:%S"),
+                    },
+                }
+            }
+            PerformHttpRequest(Config.AntiAbuse.webhook, function(err, text, headers) end, 'POST',
+                json.encode({ username = "MilanScripts | Job Center", embeds = embed }),
+                { ['Content-Type'] = 'application/json' })
             TriggerEvent(Config.AntiAbuse.customTrigger, src, 'Trigger abuse in milanscripts:setJob')
         end
         return
     end
 
+    if Config.Webhook ~= "" then
+        local embed = {
+            {
+                ["color"] = 16711680,
+                ["title"] = "Job Change",
+                ["description"] = "Player: " .. xPlayer.getName() .. "\nID: " .. src .. "\nNew Job: " .. jobName,
+                ["footer"] = {
+                    ["text"] = os.date("%Y-%m-%d %H:%M:%S"),
+                },
+            }
+        }
+        PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST',
+            json.encode({ username = "MilanScripts | Job Center", embeds = embed }),
+            { ['Content-Type'] = 'application/json' })
+    end
     xPlayer.setJob(jobName, 0)
 
     print(('[milanscripts:setJob] Player %s (ID: %d) changed job to: %s'):format(xPlayer.getName(), src, jobName))
